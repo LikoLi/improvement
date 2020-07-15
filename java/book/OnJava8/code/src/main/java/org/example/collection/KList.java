a90b2b986f2932223fe9f6d087223c8f8254abe6
liko <os-likui@jsfund.cn>
2020-07-15 21:16:15 +0800

    Collection

---
+++ java/book/OnJava8/code/src/main/java/org/example/collection/KList.java
@@ -0,0 +1,289 @@
+package org.example.collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+/**
+ * KList
+ *
+ * @author liko
+ * @date 2020/7/15
+ */
+public class KList {
+
+    public static void main(String[] args) {
+        List<User> userList = new ArrayList<>();
+        User user = new User("liko", 26);
+
+        userList.add(user);
+        userList.add(user);
+        System.out.println(userList.size());
+        System.out.println(userList.remove(user));
+        System.out.println(userList.size());
+
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(2, 3);
+        list.add(2, 5);
+        System.out.println(list);
+
+        List<Integer> subList = list.subList(1, 2);
+        subList.set(0, 0);
+
+        System.out.println(subList);
+        System.out.println(list);
+
+        System.out.println(list.containsAll(Arrays.asList(0, 4)));
+
+        System.out.println(list.retainAll(subList));
+
+        println();
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(5);
+
+        integerList.remove(5);
+    }
+
+    private static void println() {
+        System.out.println("---------------------------------------------------------------");
+    }
+}
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+class User {
+    private String name;
+    private Integer age;
+}
+
+class LikoList extends AbstractList {
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param index
+     * @throws IndexOutOfBoundsException {@inheritDoc}
+     */
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    /**
+     * Performs the given action for each element of the {@code Iterable}
+     * until all elements have been processed or the action throws an
+     * exception.  Unless otherwise specified by the implementing class,
+     * actions are performed in the order of iteration (if an iteration order
+     * is specified).  Exceptions thrown by the action are relayed to the
+     * caller.
+     *
+     * @param action The action to be performed for each element
+     * @throws NullPointerException if the specified action is null
+     * @implSpec <p>The default implementation behaves as if:
+     * <pre>{@code
+     *     for (T t : this)
+     *         action.accept(t);
+     * }</pre>
+     * @since 1.8
+     */
+    @Override
+    public void forEach(Consumer action) {
+
+    }
+
+    /**
+     * Creates a {@link Spliterator} over the elements in this list.
+     *
+     * <p>The {@code Spliterator} reports {@link Spliterator#SIZED} and
+     * {@link Spliterator#ORDERED}.  Implementations should document the
+     * reporting of additional characteristic values.
+     *
+     * @return a {@code Spliterator} over the elements in this list
+     * @implSpec The default implementation creates a
+     * <em><a href="Spliterator.html#binding">late-binding</a></em> spliterator
+     * from the list's {@code Iterator}.  The spliterator inherits the
+     * <em>fail-fast</em> properties of the list's iterator.
+     * @implNote The created {@code Spliterator} additionally reports
+     * {@link Spliterator#SUBSIZED}.
+     * @since 1.8
+     */
+    @Override
+    public Spliterator spliterator() {
+        return null;
+    }
+
+    /**
+     * Returns a sequential {@code Stream} with this collection as its source.
+     *
+     * <p>This method should be overridden when the {@link #spliterator()}
+     * method cannot return a spliterator that is {@code IMMUTABLE},
+     * {@code CONCURRENT}, or <em>late-binding</em>. (See {@link #spliterator()}
+     * for details.)
+     *
+     * @return a sequential {@code Stream} over the elements in this collection
+     * @implSpec The default implementation creates a sequential {@code Stream} from the
+     * collection's {@code Spliterator}.
+     * @since 1.8
+     */
+    @Override
+    public Stream stream() {
+        return null;
+    }
+
+    /**
+     * Returns a possibly parallel {@code Stream} with this collection as its
+     * source.  It is allowable for this method to return a sequential stream.
+     *
+     * <p>This method should be overridden when the {@link #spliterator()}
+     * method cannot return a spliterator that is {@code IMMUTABLE},
+     * {@code CONCURRENT}, or <em>late-binding</em>. (See {@link #spliterator()}
+     * for details.)
+     *
+     * @return a possibly parallel {@code Stream} over the elements in this
+     * collection
+     * @implSpec The default implementation creates a parallel {@code Stream} from the
+     * collection's {@code Spliterator}.
+     * @since 1.8
+     */
+    @Override
+    public Stream parallelStream() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Removes all of the elements of this collection that satisfy the given
+     * predicate.  Errors or runtime exceptions thrown during iteration or by
+     * the predicate are relayed to the caller.
+     *
+     * @param filter a predicate which returns {@code true} for elements to be
+     *               removed
+     * @return {@code true} if any elements were removed
+     * @throws NullPointerException          if the specified filter is null
+     * @throws UnsupportedOperationException if elements cannot be removed
+     *                                       from this collection.  Implementations may throw this exception if a
+     *                                       matching element cannot be removed or if, in general, removal is not
+     *                                       supported.
+     * @implSpec The default implementation traverses all elements of the collection using
+     * its {@link #iterator}.  Each matching element is removed using
+     * {@link Iterator#remove()}.  If the collection's iterator does not
+     * support removal then an {@code UnsupportedOperationException} will be
+     * thrown on the first matching element.
+     * @since 1.8
+     */
+    @Override
+    public boolean removeIf(Predicate filter) {
+        return false;
+    }
+
+    /**
+     * Replaces each element of this list with the result of applying the
+     * operator to that element.  Errors or runtime exceptions thrown by
+     * the operator are relayed to the caller.
+     *
+     * @param operator the operator to apply to each element
+     * @throws UnsupportedOperationException if this list is unmodifiable.
+     *                                       Implementations may throw this exception if an element
+     *                                       cannot be replaced or if, in general, modification is not
+     *                                       supported
+     * @throws NullPointerException          if the specified operator is null or
+     *                                       if the operator result is a null value and this list does
+     *                                       not permit null elements
+     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @implSpec The default implementation is equivalent to, for this {@code list}:
+     * <pre>{@code
+     *     final ListIterator<E> li = list.listIterator();
+     *     while (li.hasNext()) {
+     *         li.set(operator.apply(li.next()));
+     *     }
+     * }</pre>
+     * <p>
+     * If the list's list-iterator does not support the {@code set} operation
+     * then an {@code UnsupportedOperationException} will be thrown when
+     * replacing the first element.
+     * @since 1.8
+     */
+    @Override
+    public void replaceAll(UnaryOperator operator) {
+
+    }
+
+    /**
+     * Sorts this list according to the order induced by the specified
+     * {@link Comparator}.
+     *
+     * <p>All elements in this list must be <i>mutually comparable</i> using the
+     * specified comparator (that is, {@code c.compare(e1, e2)} must not throw
+     * a {@code ClassCastException} for any elements {@code e1} and {@code e2}
+     * in the list).
+     *
+     * <p>If the specified comparator is {@code null} then all elements in this
+     * list must implement the {@link Comparable} interface and the elements'
+     * {@linkplain Comparable natural ordering} should be used.
+     *
+     * <p>This list must be modifiable, but need not be resizable.
+     *
+     * @param c the {@code Comparator} used to compare list elements.
+     *          A {@code null} value indicates that the elements'
+     *          {@linkplain Comparable natural ordering} should be used
+     * @throws ClassCastException            if the list contains elements that are not
+     *                                       <i>mutually comparable</i> using the specified comparator
+     * @throws UnsupportedOperationException if the list's list-iterator does
+     *                                       not support the {@code set} operation
+     * @throws IllegalArgumentException      (<a href="Collection.html#optional-restrictions">optional</a>)
+     *                                       if the comparator is found to violate the {@link Comparator}
+     *                                       contract
+     * @implSpec The default implementation obtains an array containing all elements in
+     * this list, sorts the array, and iterates over this list resetting each
+     * element from the corresponding position in the array. (This avoids the
+     * n<sup>2</sup> log(n) performance that would result from attempting
+     * to sort a linked list in place.)
+     * @implNote This implementation is a stable, adaptive, iterative mergesort that
+     * requires far fewer than n lg(n) comparisons when the input array is
+     * partially sorted, while offering the performance of a traditional
+     * mergesort when the input array is randomly ordered.  If the input array
+     * is nearly sorted, the implementation requires approximately n
+     * comparisons.  Temporary storage requirements vary from a small constant
+     * for nearly sorted input arrays to n/2 object references for randomly
+     * ordered input arrays.
+     *
+     * <p>The implementation takes equal advantage of ascending and
+     * descending order in its input array, and can take advantage of
+     * ascending and descending order in different parts of the same
+     * input array.  It is well-suited to merging two or more sorted arrays:
+     * simply concatenate the arrays and sort the resulting array.
+     *
+     * <p>The implementation was adapted from Tim Peters's list sort for Python
+     * (<a href="http://svn.python.org/projects/python/trunk/Objects/listsort.txt">
+     * TimSort</a>).  It uses techniques from Peter McIlroy's "Optimistic
+     * Sorting and Information Theoretic Complexity", in Proceedings of the
+     * Fourth Annual ACM-SIAM Symposium on Discrete Algorithms, pp 467-474,
+     * January 1993.
+     * @since 1.8
+     */
+    @Override
+    public void sort(Comparator c) {
+
+    }
+}
--- java/util/easyexcel/code/src/main/java/org/liko/improvement/easyexcel/controller/EasyExcelController.java
+++ java/util/easyexcel/code/src/main/java/org/liko/improvement/easyexcel/controller/EasyExcelController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,17 +19,32 @@ public class EasyExcelController {
 
     @RequestMapping("/export")
     public void export(HttpServletResponse response) throws IOException {
-        List<User> userList = new ArrayList<>();
         User user = new User("liko", "liko");
         User user1 = new User("test", "test");
+        List<User> userList = Arrays.asList(user, user1);
+
 
         response.setContentType("application/vnd.ms-excel");
         response.setCharacterEncoding("UTF-8");
-        response.setHeader("Content-Disposition", "attachment; filename=\"test.xlsx\"");
+        String fileName = URLEncoder.encode("测试", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment; filename=" + fileName + ".xlsx");
         EasyExcel.write(response.getOutputStream(), User.class)
-                .autoCloseStream(Boolean.FALSE)
                 .sheet("test")
                 .doWrite(userList);
+
+//
+//        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
+//        response.setContentType("application/vnd.ms-excel");
+//        response.setCharacterEncoding("utf-8");
+//        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+//        String fileName = URLEncoder.encode("测试", "UTF-8");
+//        response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+//        EasyExcel.write(response.getOutputStream(), DownloadData.class).sheet("模板").doWrite(data());
+
+    }
+
+    @RequestMapping("/test")
+    public void test() {
 
     }
 }
